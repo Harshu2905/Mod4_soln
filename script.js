ebc90cb77a716e2c32c82a7b8203b20305afe30b
Lakshmi Harshitha <Lakshmi Harshitha>
2017-02-05 18:03:31 +0530

    script.js

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 (function(window){
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for ( var i=0; i<names.length; i++) {
-		var name = names[i].toLowerCase();
+		
 		var firstLetter =names[i].charAt(0);
-			if (firstLetter === 'j') {
+			if (firstLetter === 'j' || firstLetter === 'J') {
     			byeSpeaker.speak(names[i]);
   				} else {
     			helloSpeaker.speak(names[i]);
